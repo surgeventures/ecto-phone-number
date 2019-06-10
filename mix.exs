0976a8f5a16f2b69518408aa9882e24d63a11b0f
Piotr Kowalski <piotr.kowalski@shedul.com>
2019-06-10 15:13:09 +0200

    [ID-7973] Use original ex_phone_number package

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoPhoneNumber.Mixfile do
 
   def project do
     [app: :ecto_phone_number,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
@@ -49,7 +49,7 @@ defmodule EctoPhoneNumber.Mixfile do
       {:ecto, ">= 2.0.0"},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:excoveralls, "~> 0.7", only: :test},
-      {:ex_phone_number, github: "surgeventures/ex_phone_number_new", tag: "v1.0"},
+      {:ex_phone_number, "~> 0.2"},
     ]
   end
 
