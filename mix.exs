e14ea83589e455b5f4c35e6bd509de94dbb03f59
Karol Słuszniak <ksluszniak@gmail.com>
2019-07-04 11:54:14 +0200

    Refine project config

--- mix.exs
+++ mix.exs
@@ -1,63 +1,45 @@
 defmodule EctoPhoneNumber.Mixfile do
   use Mix.Project
 
-  def project do
-    [app: :ecto_phone_number,
-     version: "0.2.0",
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     package: package(),
-     aliases: aliases(),
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: [
-       check: :test,
-       coveralls: :test,
-       "coveralls.detail": :test,
-       "coveralls.html": :test],
-     name: "EctoPhoneNumber",
-     description: "Phone number type with validation and formatting for Ecto",
-     source_url: "https://github.com/surgeventures/ecto-phone-number",
-     homepage_url: "https://github.com/surgeventures/ecto-phone-number",
-     docs: [main: "readme",
-            extras: ["README.md"]]]
-  end
+  @name "EctoPhoneNumber"
+  @description "Phone number type with validation and formatting for Ecto"
+  @github_url "https://github.com/surgeventures/ecto-phone-number"
 
-  defp package do
-    [maintainers: ["Karol Słuszniak"],
-     licenses: ["MIT"],
-     links: %{
-       "GitHub" => "https://github.com/surgeventures/ecto-phone-number",
-     },
-     files: ~w(mix.exs lib LICENSE.md README.md)]
+  def project do
+    [
+      app: :ecto_phone_number,
+      version: "0.2.0",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      name: @name,
+      description: @description,
+      package: [
+        maintainers: ["Karol Słuszniak"],
+        licenses: ["MIT"],
+        links: %{
+          "GitHub" => @github_url
+        }
+      ],
+      docs: [
+        main: "readme",
+        extras: ["README.md"],
+        source_url: @github_url
+      ]
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
-
-  defp aliases do
-    [
-      "check": check_alias(),
-    ]
-  end
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
-      {:credo, "~> 0.8.1", only: [:dev, :test]},
+      {:credo, "~> 0.8.1", only: :dev, runtime: false},
       {:ecto, ">= 2.0.0"},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.7", only: :test},
-      {:ex_phone_number, "~> 0.2"},
-    ]
-  end
-
-  defp check_alias do
-    [
-      "compile --warnings-as-errors --force",
-      "test",
-      "credo --strict",
+      {:ex_phone_number, "~> 0.2"}
     ]
   end
 end
