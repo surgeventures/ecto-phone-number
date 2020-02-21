c683981827b0f709a90957e157748ec3e826d154
marcin piotr miszczyk <mpmiszczyk@gmail.com>
2020-02-21 12:59:08 +0100

    Update dependency to correctly validate newly registered numbers

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule EctoPhoneNumber.Mixfile do
       {:credo, "~> 1.0", only: :dev, runtime: false},
       {:ecto, ">= 2.0.0"},
       {:ex_doc, "~> 0.20", only: :dev, runtime: false},
-      {:ex_phone_number, "~> 0.2"}
+      {:ex_phone_number, github: "surgeventures/ex_phone_number", tag: "v0.2.1"}
     ]
   end
 end
