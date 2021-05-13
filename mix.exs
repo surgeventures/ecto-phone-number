e30ae1e5e3d90ea24342d452ea8526e5aa9e110a
marcin piotr miszczyk <mpmiszczyk@gmail.com>
2021-05-13 15:15:09 +0200

    Release new package version.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule EctoPhoneNumber.Mixfile do
   def project do
     [
       app: :ecto_phone_number,
-      version: "0.3.4",
+      version: "0.3.5",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
