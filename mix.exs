e8eae55c83d5811a4361e54697ab7d2417a6ba78
Mariusz Cieniewski <mariusz@cieniew.ski>
2021-06-07 15:02:38 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule EctoPhoneNumber.Mixfile do
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ecto, ">= 2.0.0"},
       {:ex_doc, "~> 0.24.2", only: :dev, runtime: false},
-      {:ex_phone_number, "0.0.1-git-dc9e", organization: "fresha"}
+      {:ex_phone_number, "~> 0.0.2", organization: "fresha"}
     ]
   end
 end
